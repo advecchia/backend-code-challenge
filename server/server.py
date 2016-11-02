8d84b8f4aeca829b1893977e67f28ce574f6eb1a
advecchia <simagicall@gmail.com>
2016-11-02 11:43:53 -0200

    Changing default attributes for flask app

--- server/server.py
+++ server/server.py
@@ -350,6 +350,6 @@ def handle_invalid_input(error):
     return response
 
 if __name__ == '__main__':
-    host = '127.0.0.1'
-    port = int(os.environ.get('PORT', 5000))
+    host = '0.0.0.0'
+    port = int(os.environ.get('$PORT', 5000))
     app.run(threaded=True, debug=True, host=host, port=port)
